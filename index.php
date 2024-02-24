dd237e69f19989520285547a3a71cf754278b848
@ProducerWedge <153241086+ProducerWedge@users.noreply.github.com>
2024-02-24 12:10:53 -0600

    Update index.php

--- index.php
+++ index.php
@@ -68,11 +68,11 @@
         </style>
     </head>
     <body>
-     
+
         <div class="container">
-           <img width:40px src="picture.jpg"></img>
+            <img width:40px src="picture.jpg"></img>
             <h1>Welcome to the Neighborhood</h1>
-            <p class="author">Storys by Rick Mace</p>
+            <p class="author">Stories by Rick Mace</p>
           <h1>Lawnmower</h1>
             <p>The day began by being torn from my sleep by the sound of my neighbor’s Ferris IS6200 Zero Turn Mower. His name was Frank, he’s what some called an eccentric lunatic. He enjoyed the smell of a Camellia Japonica at dawn and a French machine gun being fired in pitch dark. A large man, with a beard consisting of splintered grey and black hairs, bifocals and always adorned in a bolo tie. The tie had a centerpiece that contained a small compartment that held his middle finger’s distal phalanx, or tip. He lost it in a bowling accident during a late-night high-stakes tournament with the Lucky Strikes, a team of four delinquents that were known to put super glue in their opponent's bowling ball sockets. When Frank bowled the last frame for the win the tip of his middle finger was ripped off as the ball continued down the lane for a strike and the win. Later that night he distinguished his lit lucky strike cigarette in the eye socket of the leader of the Strikes for payback and the actual win.</p>
             <p> A riding lawnmower was overkill due to Frank’s yard being the size of a diorama of a real suburban yard. The first-day inauguration of his mower, three weeks ago, had consisted of him running over a sprinkler head and ripping out a large section of his sprinkler system as it became lodged under the frame. In the confusion, he plummeted off the curb and continued down his cul-de-sac dragging his sprinkler system with him. When he turned back to see the damage, he tumbled off his mower. He hit with a loud thud as his zero-turn aimlessly rolled to the curb and stopped. He laid on his back feeling the warmness of the concrete and stared into the cold blue sky as his past failures and misfortunes flowed abruptly through him. He clutched the end of his bolo tie for strength and angrily pulled himself up.</p>
@@ -119,6 +119,36 @@
               <input type="submit" name="Submit" value="Submit Comment" class="Submit">
             </form>
         </div>
+      <div class="container">
+        <h1>Runaway Monkey</h1>
+          <p>Two weeks ago, a person moved into the house across the street from us in Steeplechase. She was from Peru and had worked for Cisco Systems as a software engineer. My wife and I went over one day to say hello and introduce ourselves. She seemed very pleasant and invited us in. We took a seat in the living room on a tan Herman Miller sectional couch. She sat across from us on a Magic Spun lounge chair in chocolate.</p>
+          <p> As we made polite conversation, I could hear some noises coming from the kitchen. I wondered if that might be her husband. She snapped her fingers and the door to the kitchen flew open to reveal an enormous albino monkey. He was wearing a little yellow beanie that barely fit his huge oblong head and his razor-sharp fangs angrily protruded from its mouth. His pink eyes darted back and forth between my wife and me as if at any second, he would attack and maul us to death. He was carrying a tray of hors d'oeuvres and small glasses of velvet liqueur. I jumped back in fear as he released an ear-piercing whooping sound and angrily dropped the tray on an Arhaus Telluride coffee table in wood and was gone before I could take another breath.</p>
+          <p>She smiled as she spoke, “I found him deep in the bowels of the Amazon jungle. Isn’t El Pedo wonderful?” I looked around. “Was this real”, I thought.  Had she hypnotized us with some kind of AI voodoo? Was I going to wake up under an overpass feeling like I had been probed by some overzealous aliens? She grabbed the small glass of liqueur as she spoke, “I taught him to do chores, cook, and help around the house.” The rest of the conversation was a blur, all I could think about was El Pedo.</p>
+        <p>A week later it was a Thursday night, and I was taking the trash out to the street. I looked over to see the garage door to our new neighbor’s house coming up. The car backed out and when I looked in El Pedo was behind the wheel. He rolled the window down and held up a fifty-dollar bill and an empty box of Marlboro cigarettes. He motioned for me to come to the car. When I came closer, I heard the click of the car door unlock. He made a snapping sound with his hand and motioned once more with the money and empty box. I knew what he wanted. I shook my head no. He pulled out another fifty. I figured you only live once, so let’s go!</p>
+        <p>
+          El Pedo was a good driver. He sat in a Dora the Explorer booster seat and had a thin block of wood bungee corded to his right leg to reach the pedals. After picking up his smokes we drove through the dark burrows of Houston. He would point and click his teeth as he showed me places never seen or appreciated before now. I was seeing them in a new light a new point of view. Everything looked special because my driver or better yet, spiritual guide, was an albino monkey named El Pedo. I told him about my family as I handed him my phone to show him pictures of my wife and kids. He looked once and then threw it out the window. There was no turning back now, no safety belt or rip cord to pull.
+        </p>
+        <p>
+          As we made our way down the two-nine-zero, we smoked Black Dragon cigars and drank banana-flavored rum until we were numb. El Pedo was heavy-footed and seemed to like to always keep the petal to the metal. Throttle juice flowed like a river bursting through a collapsing dam causing us to stay at a constant speed well over the limit. El Pedo also liked to tailgate and then bump the car in front of us.  Drivers would abruptly veer over and then come at us in rage. Their eyes would bulge from their faces when they looked over to see an albino monkey driving. I would politely give them a “Howdy Folks” wave as they swerved off the road and slammed on their brakes. We would laugh as if there was no tomorrow every time it happened.
+        </p>
+        <p>
+          We continued into the darkness with profound anticipation and a keen awareness of what might happen next. El Pedo pulled a picture he had hidden under his fur and handed it to me. It was of another albino monkey standing next to the bronze Buc-ee’s Beaver statue. There was a large X across Buc-ee’s face. I looked over to see a tear in his eye. I knew at that moment the addle dysfunctional beaver had somehow stolen his love. The happiness then slowly turned to whispers of melancholy as we rolled the windows down and felt the cool breeze flow over us as Miles Davis’s Kinda Blue spilled from the speakers. We drove until dawn.
+        </p>
+        <p>
+          The next day I came outside of my house to see two police cars across the street. The neighbor was frantic, someone she believed had stolen her car. I knew at that moment that El Pedo was gone. I looked down to see a yellow beanie and a fresh pack of Marlboros at my feet. Goodbye my friend, I will miss you! Hang on a second, where’s my wallet?
+        </p>
+        
+
+          <form action="#" method="POST">
+            <label> Name: 
+              <input type="text" name="Name" class="Input" style="width: 225px" required>
+             </label>
+            <label> Comment:
+              <textarea id="message" name="Comment" rows="4" required></textarea>
+            </label>
+            <input type="submit" name="Submit" value="Submit Comment" class="Submit">
+          </form>
+      </div>
     <html>
    
    <br><br>
