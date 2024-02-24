87a90a960531f6943feefbb6c4994ea18482fb51
Matthew Mace <105390001+openexclusiveegg@users.noreply.github.com>
2024-02-24 12:02:42 -0600

    a
    a

--- index.php
+++ index.php
@@ -39,6 +39,9 @@
           font-size:16px;
           color: #333;
       }
+      img {
+      width: 800px;
+      }
             label {
                 display: block;
                 margin: 10px 0;
@@ -65,12 +68,47 @@
         </style>
     </head>
     <body>
+     
         <div class="container">
-            <h1>Welcome to my Neighborhood</h1>
-            <p class="author">By Rick Mace</p>
-            <p>Next, As Mr. Cracker passed by Mrs. Chink's house, he noticed her struggling to carry a bag of half eaten dogs. Without hesitation, he offered his assistance. "Good morning, Mrs. Chink! Lovely day, isn't it? Allow me to lend a hand with those half eaten dogs you have there!"</p>
-            <p>Next, As Mr. Cracker passed by Mrs. Chink's house, he noticed her struggling to carry a bag of half eaten dogs. Without hesitation, he offered his assistance. "Good morning, Mrs. Chink! Lovely day, isn't it? Allow me to lend a hand with those half eaten dogs you have there!"</p>
-            <p>Finally, As Mr. Cracker passed by Mrs. Chink's house, he noticed her struggling to carry a bag of half eaten dogs. Without hesitation, he offered his assistance. "Good morning, Mrs. Chink! Lovely day, isn't it? Allow me to lend a hand with those half eaten dogs you have there!"</p>
+           <img width:40px src="picture.jpg"></img>
+            <h1>Welcome to the Neighborhood</h1>
+            <p class="author">Storys by Rick Mace</p>
+          <h1>Lawnmower</h1>
+            <p>The day began by being torn from my sleep by the sound of my neighbor’s Ferris IS6200 Zero Turn Mower. His name was Frank, he’s what some called an eccentric lunatic. He enjoyed the smell of a Camellia Japonica at dawn and a French machine gun being fired in pitch dark. A large man, with a beard consisting of splintered grey and black hairs, bifocals and always adorned in a bolo tie. The tie had a centerpiece that contained a small compartment that held his middle finger’s distal phalanx, or tip. He lost it in a bowling accident during a late-night high-stakes tournament with the Lucky Strikes, a team of four delinquents that were known to put super glue in their opponent's bowling ball sockets. When Frank bowled the last frame for the win the tip of his middle finger was ripped off as the ball continued down the lane for a strike and the win. Later that night he distinguished his lit lucky strike cigarette in the eye socket of the leader of the Strikes for payback and the actual win.</p>
+            <p> A riding lawnmower was overkill due to Frank’s yard being the size of a diorama of a real suburban yard. The first-day inauguration of his mower, three weeks ago, had consisted of him running over a sprinkler head and ripping out a large section of his sprinkler system as it became lodged under the frame. In the confusion, he plummeted off the curb and continued down his cul-de-sac dragging his sprinkler system with him. When he turned back to see the damage, he tumbled off his mower. He hit with a loud thud as his zero-turn aimlessly rolled to the curb and stopped. He laid on his back feeling the warmness of the concrete and stared into the cold blue sky as his past failures and misfortunes flowed abruptly through him. He clutched the end of his bolo tie for strength and angrily pulled himself up.</p>
+            <p>Frank was my friend. We were more different than alike. He was the down to my up. I was the shallow comment to his deep thoughts. We met on the highway in a road rage event. I had cut him off without signaling. He had followed me for twenty miles firing chrome-plated ball bearings at my car with his Pro X Slingshot. I finally got tired and just pulled over. We pounded, thrashed, and thumped each other until we grew to like one another. Shaking hands, I realized he was my neighbor.</p>
+          <p>I looked out the window to see the mower abruptly stop. Frank angrily banged on the handles. He continued to hit the starter to no avail. I wiped my eyes, grabbed some clothes, and went out to help. As I walked across the street a woman, who appeared to be inside out from her lack of fashion sense, ate a pinwheel sandwich as her Neapolitan Mastiff dropped a load in an unsuspecting yard. She smiled, took another large bite, and then kicked it into the street like Messi kicking a winning goal. I watched her walk away as I entered Frank’s yard.</p>
+          <p>“Franko, how it goes?” He looked up, “What doesn’t kill you, makes you wish it did!” I looked at the mower as if I might know what was wrong before speaking.
+          “Check the sparkplug?”
+          “Not yet. How do you check it?”</p>
+          <p>
+            I had no idea but couldn’t resist. “It’s a strange method but I saw it on YouTube and read about it in Modern Mechanics. Not sure if you want to do it.”
+                      He angrily replied, “Get on with it! What do you do?”
+            “One person hits the starter while the other urinates on the sparkplug. If you see blue smoke the sparkplug is good, red it’s bad.”
+            “Screw you. I’m not trying that. I bet it’s the fuel filter.”
+            “Maybe, it’s up to you,” I said as if not caring.
+          </p>
+          <p>
+            Frank looked around the yard in hopes of a better idea. Finding none he spoke, “I’ll hit the starter. You do the honors.”
+            “I’m colored blind, you’ll have to do it.”
+            “Colored blind, what does that have to do with it?”
+            “I can’t see the color of the smoke and you can’t see it from where you are sitting.”
+          </p>
+          <p>
+            He indignantly pulled himself off the mower. We passed each other as I climbed on the seat, and he removed the sparkplug cap. He looked around the neighborhood before unzipping. Frank let it go. I hit the starter. The electric current crackled and popped as it quickly flowed upstream like a run of salmon on their way upriver. The sodium and uric acid delivered the unwanted parcel of electricity quickly and succinctly like an overzealous FedEx driver. The explosive punch to the crotch caused him to scream three octaves higher than normal in pain. He began rolling around the yard. “Tuck and roll. Tuck and roll,” I yelled as the aroma of burning flesh filled our noses.
+            I knew my only hope was to run. I leapt off the mower and ran for my house. He followed in a serpentine pattern as he gathered his equilibrium. I ran into my house and slammed the door behind me. Frank rode the closed door into the front hallway as it was ripped from its hinges.
+          </p>
+          <p>
+            We entered the kitchen and as we passed through the dining room my mother-in-law, late seventies, blueish grey hair, and an overall appearance of sternness looked up from her tea. My two kids and wife sat at the dining room table in front of their full plates looking on in bewilderment.
+            “Sit!” She yelled.
+          </p>
+          <p>
+            We abruptly stopped in our tracks. I reluctantly grabbed a chair next to my son. My wife stood as she spoke, “Hi Frank. Take mine.” She motioned to her plate. Frank sat. We stared at each other from across the table. We were once more kids in trouble. It felt good to be young again!
+          </p>
+          <p>
+            Frank leaned back as he spoke, “Could I get some bacon bits for my mashed potatoes?” We began eating. After fifteen minutes we were friends again. It seems all differences disappear on a full stomach.
+          </p>
+          
             <form action="#" method="POST">
               <label> Name: 
                 <input type="text" name="Name" class="Input" style="width: 225px" required>
